a6006c1d4b92c92a5acd187396eb9fafd11707a9
Polina <49097861+Kovriki@users.noreply.github.com>
2021-02-16 17:59:21 +0300

    Update js.js

--- javascript/js.js
+++ javascript/js.js
@@ -2,8 +2,8 @@ $(document).ready(function(){
   let rotate = 0;
   $(".quater").click(function(){rotate += 90;
   $(this).css({"transform": "rotate("+rotate+"deg)"
-  let count 
-  rotate==360 ? rotate = 0;
+  // let count
+  // rotate==360 ? rotate = 0;
   });
 
 });
