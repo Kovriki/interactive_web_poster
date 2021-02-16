f5923fe4245498da24397c4f35252f071e89ee8e
Polina <49097861+Kovriki@users.noreply.github.com>
2021-02-16 16:45:01 +0300

    Create js.js

---
+++ javascript/js.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+  let rotate = 0;
+  $(".quater").click(function(){rotate += 90;
+  $(this).css({"transform": "rotate("+rotate+"deg)"
+  let count 
+  rotate==360 ? rotate = 0;
+  });
+
+});
+})
