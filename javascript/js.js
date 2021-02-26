16e5ab07ced5d4a66b3fd91dd1654f13dfb35b9b
Polina <49097861+Kovriki@users.noreply.github.com>
2021-02-26 17:47:08 +0300

    Update js.js

--- javascript/js.js
+++ javascript/js.js
@@ -21,23 +21,30 @@ $(document).ready(function(){
       "grid-template-rows": "6vw auto 6vw"
     });
   });
-
   $(".conteiner_2").click(function(){
     $(".conteiner_2").toggleClass("container_fill")
     $(".square_2").toggleClass("fill")
-
   })
   $(".conteiner_4").click(function(){
     $(".conteiner_4").toggleClass("container_fill")
     $(".square_4").toggleClass("fill")
   })
 
-  $(".btn_dalee").click(function(){
-    $(".setka").addClass("")
-  })
 
+//
 
+$(".btn_dalee").click(function(){
+  $(".setka").addClass("hidden")
+  setTimeout(function(){
+    $(".setka").addClass("blocker");
+    }, 300);
 
+    setTimeout(function(){
+      $(".redactor").removeClass(" hidden");
+      $(".redactor").removeClass(" blocker");
+    }, 400);
+
+});
 //   $(".conteiner_2").hover(function(){
 //     $(".square_2").css("background-color": "black");
 //     $(".square_2").css("border-color": "whyte");
